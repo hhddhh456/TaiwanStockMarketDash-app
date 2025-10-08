f74bc008a5aefb9c3436c85c35ad6ee40a952ce5
hhddhh456 <kkeep0a165@gmail.com>
2025-10-09 06:22:51 +0800

    fix port

--- dashboard.py
+++ dashboard.py
@@ -318,4 +318,7 @@ def update_dashboard(ticker1, ticker2, year):
     return corr_info, metrics_html, corr_fig, bar_fig, sharpes_fig, maxdraw_fig
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
